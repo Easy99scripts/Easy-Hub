0999d5352be67c6cf2122221d7f6f5f84acec426
Easy99scripts <167040354+Easy99scripts@users.noreply.github.com>
2024-04-29 02:22:33 -0300

    Update EasyScripts Hub.lua

--- EasyScripts Hub.lua
+++ EasyScripts Hub.lua
@@ -1,1 +1,1 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/Easy99scripts/Teste/main/Quelorga.lua.txt"))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/Easy99scripts/Easy-2/main/BlackOn.lua"))()
